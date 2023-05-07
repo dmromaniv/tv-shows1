f06042b4d0c4426bfad56fd07761a28b4869fea9
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-05-07 21:53:53 +0300

    added basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,10 @@ import store from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.scss";
 
+const basename = window.location.pathname;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <Provider store={store}>
       <React.StrictMode>
         <App />
