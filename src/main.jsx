760fca245b6da584f91f46c9f7cd0c082c024012
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-05-07 17:34:27 +0300

    updated routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "./redux/store.js";
@@ -8,11 +8,11 @@ import App from "./App.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/tv-shows">
+  <HashRouter basename="/tv-shows">
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
